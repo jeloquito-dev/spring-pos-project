7483593ba7444aebd87244f77de0c5634e18a304
Jelo Quito <jeloquito.dev@gmail.com>
2022-09-10 12:55:45 +0800

    initial commit

---
+++ src/main/java/com/playground/springposproject/SpringPosProjectApplication.java
@@ -0,0 +1,13 @@
+package com.playground.springposproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPosProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPosProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/springposproject/SpringPosProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.springposproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPosProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
